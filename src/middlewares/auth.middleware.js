f7a2557f80c5efc04fde656b28926fd277e3bcb2
Thapa-Dipesh <114410149+Thapa-Dipesh@users.noreply.github.com>
2025-05-13 10:03:20 +0000

    auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Authentication token is missing or invalid",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      message: "Forbidden",
+    });
+  }
+};
+
+export default authMiddleware;
