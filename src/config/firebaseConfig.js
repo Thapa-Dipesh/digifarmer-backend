3e22776c97440982e04d7d61709d6ae0f15c60bd
arunchapagain <chaparun2074@gmail.com>
2025-05-13 17:18:43 +0545

    Remove databaseURL from Firebase admin initialization

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -9,7 +9,6 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}");
 // Initialize the Firebase admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 export default admin;
