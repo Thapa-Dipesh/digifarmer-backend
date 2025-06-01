d48a1a0aae691d2ddb3148a96fcb83acf418c0b9
Thapa-Dipesh <114410149+Thapa-Dipesh@users.noreply.github.com>
2025-06-01 11:29:34 +0000

    update prisma migrations: create and remove Image table

---
+++ prisma/migrations/20250516083929_create_image_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "imageUrl" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
