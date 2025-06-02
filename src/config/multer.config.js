244bf9e4eff212a7f8aad06b72fcfec984e238ae
Thapa-Dipesh <114410149+Thapa-Dipesh@users.noreply.github.com>
2025-06-02 04:21:21 +0000

    refactor multer configuration: change upload destination to a dynamic path and ensure upload directory exists

--- src/config/multer.config.js
+++ src/config/multer.config.js
@@ -2,9 +2,21 @@ import multer from "multer";
 import path from "path";
 import crypto from "crypto";
 
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define an absolute path or a path relative to the project root
+const UPLOAD_DIR = path.join(__dirname, "..", "..", "public", "uploads"); // Example: project-root/public/uploads
+
+// Ensure the upload directory exists
+fs.mkdirSync(UPLOAD_DIR, { recursive: true });
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, UPLOAD_DIR);
   },
   filename: function (req, file, cb) {
     crypto.randomBytes(8, function (err, name) {
