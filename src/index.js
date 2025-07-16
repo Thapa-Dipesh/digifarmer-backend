53e25891121f31b2e8bc35cd38f25427f76a5da5
Thapa-Dipesh <114410149+Thapa-Dipesh@users.noreply.github.com>
2025-07-16 07:01:12 +0000

    feat: add CORS middleware and update start script in package.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
